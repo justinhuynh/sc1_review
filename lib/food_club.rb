6f153979f155791a4febb8de5fc45ef6ed0778b7
justinhuynh <jhuynh@gmail.com>
2015-11-12 17:12:41 -0500

    completed review

--- lib/food_club.rb
+++ lib/food_club.rb
@@ -1,2 +1,31 @@
 class FoodClub
+  attr_reader :name, :start_date, :end_date, :foodies, :theme_dinners
+
+  def initialize(name, start_date, end_date)
+    @name = name
+    @start_date = start_date
+    @end_date = end_date
+    @foodies = []
+    @theme_dinners = []
+  end
+
+  def grand_finale
+    end_date - 1
+  end
+
+  def sign_up(foodie)
+    foodies << foodie
+  end
+
+  def schedule(theme_dinner)
+    theme_dinners << theme_dinner
+  end
+
+  def member_list
+    output = "Food Club: #{name}"
+    foodies.each do |foodie|
+      output += "#{foodie.name} #{foodie.email}"
+    end
+    output
+  end
 end
